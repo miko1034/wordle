6b8bcda486f01c8d2a0110629d23de2ad5009370
miko1034 <84585119+miko1034@users.noreply.github.com>
2024-02-09 22:44:00 +0000

    console wordle kinda works

--- consoleWordle.py
+++ consoleWordle.py
@@ -1,4 +1,4 @@
-from colorama import Fore
+from termcolor import colored
 from func import *
 
 secretWord = getRandomWord()
@@ -16,6 +16,18 @@ while guesses > 0:
             break
         else:
             fragmentedGuess = list(usrinp)
-            fragmentedsecret = list(secretWord)
+            output = []
+            for i in range(len(fragmentedGuess)):
+                if fragmentedGuess[i] in positions[0]:
+                    output.append(colored(f"{fragmentedGuess[i]}", "green"))
+                elif fragmentedGuess[i] in positions[1]:
+                    output.append(colored(f"{fragmentedGuess[i]}", "yellow"))
+                else:
+                    output.append(colored("*", 'light_grey'))
+            output = "".join(output)
+            print(output)
     else:
         print("invalid input")
+
+#ok partially works. kinda broken when word is (for example) "risky" and you guess risks
+#then it kinda breaks... but we move
